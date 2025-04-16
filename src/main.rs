a1dc5af8d302fcdb7c44588f81faf9e7f682f79f
Lukas <lukas.hertel@thalesgroup.com>
2025-04-16 08:25:30 +0200

    Even smaller

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,2 @@
 #[tokio::main(flavor = "current_thread")]
-async fn main() {
-    // Comment me to get "module does not export `cabi_realloc`"
-    let _ = getrandom::u64().unwrap();
-}
+async fn main() {}
